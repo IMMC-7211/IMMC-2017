52aaacfee48e9187a6e691110b34160b9e20b862
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2017-04-12 18:13:03 -0500

    Matrix generation code
    generates numpy matrices from flight price data

--- matrix_gen.py
+++ matrix_gen.py
@@ -1,1 +1,14 @@
-import numpy
+import numpy as np
+import json
+
+f1 = open("LAXtoNRT.txt", "r") # LAX to NRT
+f2 = open("JFKtoLHR.txt", "r") # JFK to LHR
+
+data1 = json.loads(f1.read())
+data2 = json.loads(f2.read())
+
+f1.close()
+f2.close()
+
+matrix1 = np.matrix(data1)
+matrix2 = np.matrix(data2)
