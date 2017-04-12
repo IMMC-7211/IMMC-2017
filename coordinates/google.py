1a0645bd5ec5c70237fba1d865e9ef79a93ac012
nkim9005 <nkim9005@gmail.com>
2017-04-12 12:49:36 -0500

    changed variables and printed lat and lng

--- coordinates/google.py
+++ coordinates/google.py
@@ -3,23 +3,25 @@ with open('Cities.txt') as f:
     content = f.read().splitlines()
 content.remove('\ufeffTokyo')
 content.append('Tokyo')
-count = 0
 for site in content[:]:
     content.remove(site)
     content.append(site.replace(" ", "+"))
 for site in content[:]:
     if site == '':
         content.remove(site)
-    count += 1
 test = ['Chicago', 'New+York', 'Atlanta']
 coordinates = {}
 for city in content:
     r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=AIzaSyDrMWc153_fGhQhmmsYLOzJUKNAp3kFBJ8')
     
-    fag = r.json()
-    fag2 = fag['results']
-    fag3 = fag2[0]
-    fag4 = fag3['geometry']
-    fag5 = fag4['location']
+    info_all = r.json()
+    results = info_all['results']
+    list_item = results[0]
+    geometry = list_item['geometry']
+    location = geometry['location']
+    lat = location['lat']
+    lng = location['lng']
+
     print(city +  ': ')
-    print(fag5)
+    print(lat)
+    print(lng)
